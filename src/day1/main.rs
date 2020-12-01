b4d14862364f100c5934e67529c812e46415bd40
Joe Shaw <joe@joeshaw.org>
2020-12-01 10:46:21 -0500

    day1

---
+++ src/day1/main.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() {
+    let file = File::open("src/day1/input.txt").expect("unable to open input.txt");
+    let reader = BufReader::new(file);
+
+    let vals: Vec<i32> = reader
+        .lines()
+        .map(|l| l.unwrap().parse().expect("expected an integer"))
+        .collect();
+
+    'outer1: for x in &vals {
+        for y in &vals {
+            if x + y == 2020 {
+                println!("{}", x * y);
+                break 'outer1;
+            }
+        }
+    }
+
+    'outer2: for x in &vals {
+        for y in &vals {
+            for z in &vals {
+                if x + y + z == 2020 {
+                    println!("{}", x * y * z);
+                    break 'outer2;
+                }
+            }
+        }
+    }
+}
