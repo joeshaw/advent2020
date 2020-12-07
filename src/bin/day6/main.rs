13e453bb6dbd381de871763880ba315986fff175
Joe Shaw <joe@joeshaw.org>
2020-12-06 21:38:25 -0500

    day6

---
+++ src/bin/day6/main.rs
@@ -0,0 +1,33 @@
+// day 6
+
+use std::collections::HashSet;
+
+fn main() {
+    let contents =
+        std::fs::read_to_string("src/bin/day6/input.txt").expect("unable to read input.txt");
+
+    // part 1
+    let v: usize = contents
+        .trim()
+        .clone()
+        .split("\n\n")
+        .map(|g| g.replace("\n", "").chars().collect::<HashSet<_>>().len())
+        .sum();
+    println!("{}", v);
+
+    // part 2
+    let v: usize = contents
+        .trim()
+        .split("\n\n")
+        .map(|g| {
+            g.split("\n")
+                .map(|p| p.chars().collect::<HashSet<_>>())
+                .fold(
+                    "abcdefghijklmnopqrstuvwxyz".chars().collect::<HashSet<_>>(),
+                    |acc, x| acc.intersection(&x).cloned().collect(),
+                )
+                .len()
+        })
+        .sum();
+    println!("{:?}", v);
+}
